633f2a1a386c21d4ffe0d2af0f5012d7f57d25b6
JPablo NB <jpablo.javier.nb@gmail.com>
2021-04-04 19:14:05 -0400

    Se dividieron los perros y gatos en archivos independientes

---
+++ js/gatos.js
@@ -0,0 +1,41 @@
+export default [
+	{
+		nombre: 'Ginger',
+		imagen: '../img/gatos/ginger.jpg',
+		edad: 3,
+		raza: 'Michi',
+		sexo: 'Macho',
+		tamano: 'B',
+		se_adapta: 'No',
+		con_ninos: 'No',
+		con_perros: 'Sí',
+		con_gatos: 'No',
+		energia: 'Alta'
+	},
+	{
+		nombre: 'Tommy',
+		imagen: '../img/gatos/tommy.png',
+		edad: 2,
+		raza: 'Michi',
+		sexo: 'Macho',
+		tamano: 'M',
+		se_adapta: 'Sí',
+		con_ninos: 'Sí',
+		con_perros: 'Sí',
+		con_gatos: 'No',
+		energia: 'Baja'
+	},
+	{
+		nombre: 'Lucifer',
+		imagen: '../img/gatos/lucifer.png',
+		edad: 2,
+		raza: 'Michi',
+		sexo: 'Macho',
+		tamano: 'S',
+		se_adapta: 'Sí',
+		con_ninos: 'Sí',
+		con_perros: 'Sí',
+		con_gatos: 'Sí',
+		energia: 'Alta'
+	}
+];
