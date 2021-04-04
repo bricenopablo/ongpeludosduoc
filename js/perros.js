d510638acae038ebde8ccc2cbb42c3c252faf9c8
JPablo NB <jpablo.javier.nb@gmail.com>
2021-04-04 19:16:53 -0400

    Se arreglaron los perros y gatos

--- js/perros.js
+++ js/perros.js
@@ -1,41 +1,41 @@
 export default [
 	{
-		nombre: 'Ginger',
-		imagen: '../img/gatos/ginger.jpg',
-		edad: 3,
-		raza: 'Michi',
-		sexo: 'Macho',
-		tamano: 'B',
-		se_adapta: 'No',
-		con_ninos: 'No',
-		con_perros: 'Sí',
-		con_gatos: 'No',
-		energia: 'Alta'
-	},
-	{
-		nombre: 'Tommy',
-		imagen: '../img/gatos/tommy.png',
-		edad: 2,
-		raza: 'Michi',
-		sexo: 'Macho',
-		tamano: 'M',
-		se_adapta: 'Sí',
-		con_ninos: 'Sí',
-		con_perros: 'Sí',
-		con_gatos: 'No',
-		energia: 'Baja'
-	},
-	{
-		nombre: 'Lucifer',
-		imagen: '../img/gatos/lucifer.png',
-		edad: 2,
-		raza: 'Michi',
+		nombre: 'Flaca',
+		imagen: '../img/perros/flaca.png',
+		edad: 8,
+		raza: 'Terrier',
 		sexo: 'Macho',
 		tamano: 'S',
 		se_adapta: 'Sí',
 		con_ninos: 'Sí',
+		con_perros: 'No',
+		con_gatos: 'No',
+		energia: 'Media'
+	},
+	{
+		nombre: 'Niña',
+		imagen: '../img/perros/nina.png',
+		edad: 10,
+		raza: 'Toller',
+		sexo: 'Hembra',
+		tamano: 'M',
+		se_adapta: 'Sí',
+		con_ninos: 'Sí',
 		con_perros: 'Sí',
 		con_gatos: 'Sí',
-		energia: 'Alta'
+		energia: 'Baja'
+	},
+	{
+		nombre: 'Cachulo',
+		imagen: '../img/perros/cachulo.png',
+		edad: 6,
+		raza: 'Golden Retriever',
+		sexo: 'Macho',
+		tamano: 'B',
+		se_adapta: 'Sí',
+		con_ninos: 'Sí',
+		con_perros: 'Sí',
+		con_gatos: 'No',
+		energia: 'Media'
 	}
 ];
